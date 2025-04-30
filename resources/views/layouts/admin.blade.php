47286b5bae372ee1cf8090e0b887d428ed68e603
linhhat210 <lynn211025@gmail.com>
2025-04-30 18:49:10 +0700

    feat: chỉnh sửa giao diện admin

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -6,7 +6,8 @@
 * Licensed under MIT (https://github.com/coreui/coreui-free-bootstrap-admin-template/blob/main/LICENSE)
 -->
 <html lang="en">
-  <head>
+
+<head>
     <base href="./">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -14,23 +15,13 @@
     <meta name="description" content="CoreUI - Open Source Bootstrap Admin Template">
     <meta name="author" content="Łukasz Holeczek">
     <meta name="keyword" content="Bootstrap,Admin,Template,Open,Source,jQuery,CSS,HTML,RWD,Dashboard">
-    <title>CoreUI Free Bootstrap Admin Template</title>
-    <link rel="apple-touch-icon" sizes="57x57" href="{{ asset('dist/assets/favicon/apple-icon-57x57.png') }}">
-    <link rel="apple-touch-icon" sizes="60x60" href="{{ asset('dist/assets/favicon/apple-icon-60x60.png') }}">
-    <link rel="apple-touch-icon" sizes="72x72" href="{{ asset('dist/assets/favicon/apple-icon-72x72.png') }}">
-    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('dist/assets/favicon/apple-icon-76x76.png') }}">
-    <link rel="apple-touch-icon" sizes="114x114" href="{{ asset('dist/assets/favicon/apple-icon-114x114.png') }}">
-    <link rel="apple-touch-icon" sizes="120x120" href="{{ asset('dist/assets/favicon/apple-icon-120x120.png') }}">
-    <link rel="apple-touch-icon" sizes="144x144" href="{{ asset('dist/assets/favicon/apple-icon-144x144.png') }}">
-    <link rel="apple-touch-icon" sizes="152x152" href="{{ asset('dist/assets/favicon/apple-icon-152x152.png') }}">
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('dist/assets/favicon/apple-icon-180x180.png') }}">
-    <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('dist/assets/favicon/android-icon-192x192.png') }}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('dist/assets/favicon/favicon-32x32.png') }}">
-    <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('dist/assets/favicon/favicon-96x96.png') }}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('dist/assets/favicon/favicon-16x16.png') }}">
+    <title>@yield('title', 'Admin Panel')</title>
+
+    <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('logo/polyflix_title.png') }}">
+
     <link rel="manifest" href="{{ asset('dist/assets/favicon/manifest.json') }}">
     <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="msapplication-TileImage" content="assets/favicon/ms-icon-144x144.png">
+    <meta name="msapplication-TileImage" content="{{ asset('logo/polyflix_title.png') }}">
     <meta name="theme-color" content="#ffffff">
     <!-- Vendors styles-->
     <link rel="stylesheet" href="{{ asset('dist/vendors/simplebar/css/simplebar.css') }}">
@@ -43,12 +34,13 @@
     <script src="{{ asset('dist/js/color-modes.js') }}"></script>
     <link href="{{ asset('dist/vendors/@coreui/chartjs/css/coreui-chartjs.css') }}" rel="stylesheet">
 
-  </head>
-  <body>
-        <!-- SidebarSidebar -->
+</head>
+
+<body>
+    <!-- SidebarSidebar -->
     @include('admin.blocks.sidebar')
     <div class="wrapper d-flex flex-column min-vh-100">
-          <!-- Header -->
+        <!-- Header -->
         @include('admin.blocks.header')
 
         <!-- Main Content -->
@@ -56,28 +48,28 @@
 
 
 
-          <!-- Header -->
+        <!-- Header -->
         @include('admin.blocks.footer')
     </div>
     <!-- CoreUI and necessary plugins-->
-    <script src="{{ asset('dist/vendors/@coreui/coreui/js/coreui.bundle.min.js')}}"></script>
-    <script src="{{asset('dist/vendors/simplebar/js/simplebar.min.js')}}"></script>
+    <script src="{{ asset('dist/vendors/@coreui/coreui/js/coreui.bundle.min.js') }}"></script>
+    <script src="{{ asset('dist/vendors/simplebar/js/simplebar.min.js') }}"></script>
     <script>
-      const header = document.querySelector('header.header');
+        const header = document.querySelector('header.header');
 
-      document.addEventListener('scroll', () => {
-        if (header) {
-          header.classList.toggle('shadow-sm', document.documentElement.scrollTop > 0);
-        }
-      });
+        document.addEventListener('scroll', () => {
+            if (header) {
+                header.classList.toggle('shadow-sm', document.documentElement.scrollTop > 0);
+            }
+        });
     </script>
     <!-- Plugins and scripts required by this view-->
     <script src="{{ asset('dist/vendors/chart.js/js/chart.umd.js') }}"></script>
-    <script src="{{ asset('dist/vendors/@coreui/chartjs/js/coreui-chartjs.js')}}"></script>
+    <script src="{{ asset('dist/vendors/@coreui/chartjs/js/coreui-chartjs.js') }}"></script>
     <script src="{{ asset('dist/vendors/@coreui/utils/js/index.js') }}"></script>
     <script src="{{ asset('dist/js/main.js') }}"></script>
-    <script>
-    </script>
+    <script></script>
 
-  </body>
+</body>
+
 </html>
