8a5bcb1886d9aa5cdf72a33adc483f147c7bb531
ThaiDuongCoder <nguyentrungtinnn7454@gmail.com>
2025-06-17 20:59:26 +0700

    style: update suatchieu

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -224,9 +224,9 @@ class ComposerStaticInitb63c0539f69f2908b999c5a5e14fc4fb
         ),
         'phpDocumentor\\Reflection\\' => 
         array (
-            0 => __DIR__ . '/..' . '/phpdocumentor/reflection-docblock/src',
-            1 => __DIR__ . '/..' . '/phpdocumentor/type-resolver/src',
-            2 => __DIR__ . '/..' . '/phpdocumentor/reflection-common/src',
+            0 => __DIR__ . '/..' . '/phpdocumentor/reflection-common/src',
+            1 => __DIR__ . '/..' . '/phpdocumentor/reflection-docblock/src',
+            2 => __DIR__ . '/..' . '/phpdocumentor/type-resolver/src',
         ),
         'Whoops\\' => 
         array (
