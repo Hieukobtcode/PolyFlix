f164275efbc0ad89695d990d4cc703dd87101384
Hieukobtcode <hieultph49402@gmail.com>
2025-07-13 19:27:57 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -36,6 +36,7 @@ use App\Http\Controllers\Admin\VaiTroController;
 use App\Http\Controllers\SocialAuthController;
 use App\Http\Controllers\Admin\CommentController;
 use App\Http\Controllers\Admin\GiaVeController;
+use App\Http\Controllers\Admin\KhuyenMaiController as AdminKhuyenMaiController;
 use App\Http\Controllers\Admin\RequestController;
 use App\Http\Controllers\Client\LoginController;
 use App\Http\Controllers\Client\ProfileController;
@@ -213,7 +214,7 @@ Route::prefix('admin')->name('admin.')->middleware(['auth', 'admin.access', 'per
         Route::post('{khuyenMai}/assign-chi-nhanh', [KhuyenMaiController::class, 'assignToChiNhanh'])->name('assign-chi-nhanh');
     });
 
-    Route::resource('khuyen-mai', KhuyenMaiController::class);
+    Route::resource('khuyen-mai', AdminKhuyenMaiController::class);
 
     // Quản lý rạp phim
     Route::resource('rap-phim', RapphimController::class);
